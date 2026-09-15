fb375bfa8693b70ec5bbd400f777764801d7b54c
TeskeA27 <teskea27@bss.on.ca>
2026-09-15 10:57:19 -0400

    commit

---
+++ W2D1_TypeConversion.py
@@ -0,0 +1,43 @@
+
+# celsius = float(input("Enter a temperature in Celsius: "))
+# fahrenheit = (celsius * 9 / 5) + 32
+# print("Temperature in Fahrenheit:", fahrenheit)
+
+
+# age = int(input("Enter in your age in years: "))
+# ageindays = (age * 365)
+# result = f"You are {ageindays} days old!"
+# print (result)
+
+
+# price = float(input("Enter in a price of an item: "))
+# tax = (price * 0.13)
+# total = (price * 0.13) + price
+# taxresult = f"The tax comes up to ${tax:.2f}."
+# totalresult = f"The total comes up to ${total:.2f}."
+# print (taxresult)
+# print (totalresult)
+
+# number = int(input("Enter in a number: "))
+# number2 = int(input("Enter in another number: "))
+# sum = number + number2
+# difference = number - number2
+# product = number * number2
+# quotient = float(number) / number2
+# print(f"Sum: {sum}")
+# print(f"Difference: {difference}")
+# print(f"Product: {product}")
+# print(f"Quotient: {quotient:.2f}")
+
+
+# length = float(input("Enter the length of the rectangle: "))
+# width = float(input("Enter the width of the rectangle: "))
+# area = length * width
+# perimeter = (2 * length) + (2 * width)
+# area = int(area)
+# perimeter = int(perimeter)
+# print(f"Area: {area}")
+# print(f"Perimeter: {perimeter}")
+
+
+
