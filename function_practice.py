bae25f2a61f8570ad154b3db881d4dbf0046aefe
TeskeA27 <teskea27@bss.on.ca>
2026-09-22 00:15:36 -0400

    UUU

---
+++ function_practice.py
@@ -0,0 +1,14 @@
+# def print_welcome():
+#     """Prints a welcome message."""
+#     print("Welcome to Python programming!")
+#     print (print_welcome)
+
+
+
+number = int(input("Enter in a number: "))
+
+def  square_number():
+    """Takes a number and squares it"""
+
+    square_number = number * 2
+    print f("The answer is") {square_number}
